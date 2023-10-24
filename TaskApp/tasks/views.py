107046296d5663701e952becd9a38ccb2f9dd7da
Hashan Athurugiriya <hashan.athurugiriya@gmail.com>
2023-10-25 00:36:46 +0530

    Add Task Function

--- TaskApp/tasks/views.py
+++ TaskApp/tasks/views.py
@@ -5,3 +5,7 @@ tasks = ["foo", "bar", "baz"]
 # Create your views here.
 def index(request):
     return render(request, 'tasks/index.html', {'tasks': tasks})
+
+#Add a new Task
+def add(request):
+    return render(request, 'tasks/add.html')
